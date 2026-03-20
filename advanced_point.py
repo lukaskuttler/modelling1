9bfe6352fc1d92d94973de2d5b0f7a1f59722113
lukaskuttler <luka.kuttler2@gmail.com>
2026-03-20 11:28:35 +0100

    up to date version 2

--- advanced_point.py
+++ advanced_point.py
@@ -1,19 +1,20 @@
 from color_point import ColorPoint
 
 class AdvancePoint(ColorPoint):
-    # class variables
+    #Class variables
     COLORS = ["red", "green", "blue", "yellow", "black", "white"]
     def __init__(self, x, y, color):
-        #check the color
+        #Check the color
         if color not in self.COLORS:
-            raise ValueError(f"Invalid color: need to be one of {self.COLORS}")
-        self.x = x
-        self.y = y
-        self.color = color
+            raise ValueError(f"Invalid color : need to be one of {self.COLORS}")
+        self._x = x
+        self._y = y
+        self._color = color
 
     @property
     def x(self):
         return self._x
+
     @property
     def y(self):
         return self._y
@@ -23,15 +24,16 @@ class AdvancePoint(ColorPoint):
         return self._color
 
     @color.setter
-    def color(self, value):
+    def color(self, value): # this is a SETTER!!
         if value not in self.COLORS:
-            raise ValueError(f"Invalid color: need to be one of {self.COLORS}")
+            raise ValueError(f"Invalid color : need to be one of {self.COLORS}")
         self._color = value
+
+
 
 
 p1 = AdvancePoint(1, 2, "red")
 print(p1)
-p2 = AdvancePoint(4, 5, "pink")
-print(p2)
+# p2 = AdvancePoint(1, 2, "pink")
 print(p1.x)
-p1.distance_origin()
+#p1.x = 3
